08487adcbb3c9e6772013b4e943960dc3386fb26
Joshua Lim <s10179502@connect.np.edu.sg>
2019-07-06 21:55:06 +0800

    more layout codes

--- app/src/main/java/com/example/s10179502_week11practical/CreateUserActivity.java
+++ app/src/main/java/com/example/s10179502_week11practical/CreateUserActivity.java
@@ -1,4 +1,13 @@
 package com.example.s10179502_week11practical;
 
-public class CreateUserActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
 }
--- app/src/main/java/com/example/s10179502_week11practical/MainActivity.java
+++ app/src/main/java/com/example/s10179502_week11practical/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.s10179502_week11practical;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    View view = findViewById(R.id.tvNewUser);
+        TextView textView = findViewById(R.id.tvNewUser);
 
-
-    public void onTouchListener(View v){
-
-
+        textView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    Intent in = new Intent(MainActivity.this, CreateUserActivity.class);
+                    startActivity(in);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
